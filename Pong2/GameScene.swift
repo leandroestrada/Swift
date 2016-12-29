13a1809424547593b87eee721cac805d43655ec1
le <leandro.a.estrada@gmail.com>
2016-12-29 17:16:49 -0200

    Adding first folder

---
+++ Pong2/GameScene.swift
@@ -0,0 +1,28 @@
+//
+//  GameScene.swift
+//  Pong2
+//
+//  Created by le on 19/12/16.
+//  Copyright © 2016 leandro. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+  
+    
+    override func didMove(to view: SKView) {
+        
+        
+    }
+    
+    
+    
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
