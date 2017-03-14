5de829c3c4a933db9190a85906d4d14b3140994f
le <leandro.a.estrada@gmail.com>
2017-03-14 12:25:04 -0300

    1

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by le on 02/03/17.
+//  Copyright © 2017 Les Company. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+        
+}
+
---
+++ SWIFTBOOK.playground/Contents.swift
@@ -0,0 +1,26 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+
+
+let apples = 3
+let oranges = 10
+let appleCount = "Eu tenho \(apples + oranges) maçãs"
+
+print(appleCount)
+print(".")
+
+
+
+var shoppingList = ["catfish", "water"]
+shoppingList[1]
+
+var occupations = [
+        "Malcolm": "captain",
+        "Kaylee": "Mechanic"
+        ]
+
