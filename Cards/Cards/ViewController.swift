9d210fdc1a540ace44d8cc887e23fbc103ab5392
leandroestrada <leandro.a.estrada@gmail.com>
2018-07-29 20:23:38 -0300

    4 cards

--- Cards/Cards/ViewController.swift
+++ Cards/Cards/ViewController.swift
@@ -7,19 +7,38 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    var flipCount = 0{
+        didSet{
+            flipCountLabel.text = "Flips: \(flipCount)"
+}
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    var emojiChoices: Array<String> = ["👻","🍔","👻","🍔"]
+    
+    @IBAction func touchCard(_ sender: UIButton){
+        flipCount += 1
+        if let cardNumber = cardButtons.index(of: sender){
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }
     }
-
-
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    func flipCard( withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji{
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 0.5781051517, blue: 0, alpha: 1)
+        } else{
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
+        }
+    }
+    
+    
 }
 
